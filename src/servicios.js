7ebe1cc1b5aee015f5e589b338c5679461eff5aa
juan <juancraft12yt@gmail.com>
2020-07-19 21:46:19 -0300

    cambia funciones de modulo

--- src/servicios.js
+++ src/servicios.js
@@ -1,24 +1,24 @@
-import { obtenerPaginaSiguienteDesdeAPI, obtenerPaginaAnteriorDesdeAPI} from "./api.js"
-import { offset } from "./manejador.js"
-import { buscarPaginaEnLS} from "./storage.js"
-import { crearListaPokemones} from "./ui.js"
+import { obtenerPaginaSiguienteDesdeAPI, obtenerPaginaAnteriorDesdeAPI } from './api.js'
+import { offset } from './manejador.js'
+import { buscarPaginaEnLS } from './storage.js'
+import { crearListaPokemones } from './ui.js'
 
-export function cargarPaginaSiguiente(){
-    try{
-        let paginaLS = buscarPaginaEnLS(offset)
-        crearListaPokemones(paginaLS.results)
-        console.log("cargo pagina siguiente desde LS")
-    }catch(e){
-        obtenerPaginaSiguienteDesdeAPI()
-    }
+export function cargarPaginaSiguiente () {
+  try {
+    const paginaLS = buscarPaginaEnLS(offset)
+    crearListaPokemones(paginaLS.results)
+    console.log('cargo pagina siguiente desde LS')
+  } catch (e) {
+    obtenerPaginaSiguienteDesdeAPI()
+  }
 }
 
-export function cargarPaginaAnterior(){
-    try{
-        let paginaLS = buscarPaginaEnLS(offset)
-        crearListaPokemones(paginaLS.results)
-        console.log("cargo pagina anterior desde ls")
-    } catch(e){
-        obtenerPaginaAnteriorDesdeAPI()
-    }
+export function cargarPaginaAnterior () {
+  try {
+    const paginaLS = buscarPaginaEnLS(offset)
+    crearListaPokemones(paginaLS.results)
+    console.log('cargo pagina anterior desde ls')
+  } catch (e) {
+    obtenerPaginaAnteriorDesdeAPI()
+  }
 }
