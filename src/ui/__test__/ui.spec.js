432cdc9b42f38cd84a6d9119a609b3feba868ce3
juan <juancraft12yt@gmail.com>
2020-07-21 21:44:21 -0300

    actualiza prubea

--- src/ui/__test__/ui.spec.js
+++ src/ui/__test__/ui.spec.js
@@ -2,6 +2,6 @@ import { mostrarCantidadPokemones} from "./ui.js"
 
 test("actualiza la cantidad de pokemones", () => {
     document.body.innerHTML = '<strong id="cantidad-pokemones"></strong>'
-    mostrarCantidadPokemones(200)
-    expect(document.querySelector("#cantidad-pokemons")).toContain(200)
+    mostrarCantidadPokemones(500)
+    expect(document.querySelector("#cantidad-pokemons")).toContain(500)
 })
