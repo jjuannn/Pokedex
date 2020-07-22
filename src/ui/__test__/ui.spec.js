daa81facfcbddbcd93ab656613094449912280c9
juan <juancraft12yt@gmail.com>
2020-07-22 20:57:19 -0300

    agrega pruebas para UI
    agrega pruebas para UI con Jest

--- src/ui/__test__/ui.spec.js
+++ src/ui/__test__/ui.spec.js
@@ -1,7 +1,36 @@
-import { mostrarCantidadPokemones} from "./ui.js"
+import { 
+    mostrarCantidadPokemones,
+    borrarPokemonesAnteriores,
+    crearListaPokemones
+} from "../ui.js"
+let cantidadPokemones = 500
+
 
 test("actualiza la cantidad de pokemones", () => {
     document.body.innerHTML = '<strong id="cantidad-pokemones"></strong>'
-    mostrarCantidadPokemones(500)
-    expect(document.querySelector("#cantidad-pokemons")).toContain(500)
+    mostrarCantidadPokemones(cantidadPokemones)
+    expect(document.querySelector("#cantidad-pokemones").textContent)
+    .toContain(cantidadPokemones)
 })
+
+test("borra los pokemones", () => {
+    document.body.innerHTML = '<div id="container"><a>bulbasaur</a></div>'
+    borrarPokemonesAnteriores()
+    expect(document.querySelector("#container").textContent).toBe("")
+})
+
+/*
+test("crea la lista de pokemones", () => {
+    document.body.innerHTML = '<div id="lista-pokemones"></div>'
+    const $listaPokemones = document.querySelector('#lista-pokemones')
+    const results = [{
+        0: {name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/"},
+        1: {name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/"},
+        2: {name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/"},
+            3: {name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/"},
+        }]
+    crearListaPokemones(results)
+    const $pokemones = document.querySelectorAll("a")
+    expect($pokemones).toBe()
+})
+*/
