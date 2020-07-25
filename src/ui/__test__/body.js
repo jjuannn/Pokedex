a0b343b63ceded6543d289f538c321befeb5a922
juan <juancraft12yt@gmail.com>
2020-07-24 22:21:06 -0300

    termina pruebas para ui
    finally, actualiza y termina las pruebas para la UI de la  pokedex

---
+++ src/ui/__test__/body.js
@@ -0,0 +1,30 @@
+export const cuerpo = `<h2> Hay <strong id="cantidad-pokemones">...</strong> pokemones en la pokedex!</h2>
+    
+<div>
+  <button id="pokemones-prev" type="button" class="btn btn-warning">Pokemones Anteriores</button>
+  <button id="pokemones-next" type="button" class="btn btn-warning">Pokemones Siguientes</button>
+
+    <div id="container">
+      <div id="lista-pokemones" class="list-group overflow-auto">
+    
+    </div>
+  </div>
+</div>
+
+<ul class="list-unstyled" id="tablaInformacionPokemones">
+  <li class="media">
+    <div class="media-body" id="bodyTabla">
+      <h5 class="datos">Peso: <strong id="peso-pokemon">¿?</strong></h5>
+      <h5 class="datos">Altura: <strong id="altura-pokemon">¿?</strong></h5>
+      <h5 class="datos">HP: <strong id="hp-pokemon">¿?</strong></h5>
+      <h5 class="datos">Ataque: <strong id="ataque-pokemon">¿?</strong></h5>
+      <h5 class="datos">Defensa: <strong id="defensa-pokemon">¿?</strong></h5>
+      <h5 class="datos">Velocidad: <strong id="velocidad-pokemon">¿?</strong></h5>
+      <h5 class="datos">Ataque Especial: <strong id="ataque-especial">¿?</strong></h5>
+      <h5 class="datos">Defensa Especial: <strong id="defensa-especial">¿?</strong> </h5>
+      <h5 class="datos" id="h5tipo">Tipo(s):</h5>
+      <img id="imagen-personaje" src="template.png">
+      <img src="" id="foto-pokemon" class="oculto" alt="...">
+      <h5 class="resultadoDatos" id="elementoNombre">Un <strong id="nombre-pokemon">...</strong> salvaje ha aparecido!</h5>
+    </div>
+  </li>`
--- src/ui/__test__/ui.spec.js
+++ src/ui/__test__/ui.spec.js
@@ -4,11 +4,17 @@ import {
     crearListaPokemones,
     actualizarInformacion
 } from "../ui.js"
+import {
+    cuerpo
+} from "./body.js"
+
+const bulbasaur = require('../../../cypress/fixtures/bulbasaur.json')
+const pagina1 = require("../../../cypress/fixtures/pagina1.json")
+
 let cantidadPokemones = 500
 
-
 test("actualiza la cantidad de pokemones", () => {
-    document.body.innerHTML = '<strong id="cantidad-pokemones"></strong>'
+    document.body.innerHTML = cuerpo
     mostrarCantidadPokemones(cantidadPokemones)
     expect(document.querySelector("#cantidad-pokemones").textContent)
     .toContain(cantidadPokemones)
@@ -22,42 +28,18 @@ test("borra los pokemones", () => {
 
 
 test("crea la lista de pokemones", () => {
-    document.body.innerHTML = '<div id="lista-pokemones"></div>'
+    document.body.innerHTML = cuerpo
     const $listaPokemones = document.querySelector('#lista-pokemones')
     
-    const rta = {"count":964,"next":"https://pokeapi.co/api/v2/pokemon/?offset=2&limit=2","previous":null,
-    "results":[{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"},
-    {"name":"ivysaur","url":"https://pokeapi.co/api/v2/pokemon/2/"}]}
-
-    crearListaPokemones(rta.results)
+    crearListaPokemones(pagina1.results)
     const $pokemones = document.getElementsByClassName("list-group-item list-group-item-action l-pokemones")
-    console.log($pokemones)
-    expect($pokemones.length).toBe(2)
+    expect($pokemones.length).toBe(20)
 })
 
 test("actualiza la informacion de un pokemon", () => {
-    document.body.innerHTML = 
-    `<strong class="tipos">Agua</strong>
-    <h5 class="resultadoDatos" id="elementoNombre">Un <strong id="nombre-pokemon">...</strong> salvaje ha aparecido!</h5>
-    <img src="" id="foto-pokemon" class="oculto" alt="...">
-    <h5 class="datos">Peso: <strong id="peso-pokemon">¿?</strong></h5>
-    <h5 class="datos">Altura: <strong id="altura-pokemon">¿?</strong></h5
-    ><h5 class="datos">HP: <strong id="hp-pokemon">¿?</strong></h5>
-    <h5 class="datos">Ataque: <strong id="ataque-pokemon">¿?</strong></h5>
-    <h5 class="datos">Defensa: <strong id="defensa-pokemon">¿?</strong></h5>
-    <h5 class="datos">Velocidad: <strong id="velocidad-pokemon">¿?</strong></h5>
-    <h5 class="datos">Ataque Especial: <strong id="ataque-especial">¿?</strong></h5>
-    <h5 class="datos">Defensa Especial: <strong id="defensa-especial">¿?</strong> </h5>
-    <h5 class="datos" id="h5tipo">Tipo(s):</h5><div></div>`
-    const $h5 = document.querySelector('#h5tipo')
+    document.body.innerHTML = cuerpo
     
-    const infoPokemon = {"weight":69,"height":7, "name": "bulbasaur", "sprites":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-    "stats": [{"hp":45},{"ataque":49},{"defensa":49},{"ataque-especial":65},{"defensa-especial":65},{"velocidad":45}],
-    "types":[{"slot":1,"type":{"name":"grass","url":"https://pokeapi.co/api/v2/type/12/"}},
-            {"slot":2,"type":{"name":"poison","url":"https://pokeapi.co/api/v2/type/4/"}}]
-    }
-
-    actualizarInformacion(infoPokemon)
+    actualizarInformacion(bulbasaur)
 
     expect(document.querySelector("#nombre-pokemon").textContent).toBe("bulbasaur")
     expect(document.querySelector("#peso-pokemon").textContent).toBe("69")
@@ -68,6 +50,5 @@ test("actualiza la informacion de un pokemon", () => {
     expect(document.querySelector("#velocidad-pokemon").textContent).toBe("45")
     expect(document.querySelector("#ataque-especial").textContent).toBe("65")
     expect(document.querySelector("#defensa-especial").textContent).toBe("65")
-
 
 })
