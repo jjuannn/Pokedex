c3c6629dc4dc13b4956a44559e369559dbe68623
juan <juancraft12yt@gmail.com>
2020-08-08 23:00:19 -0300

    Revert "agrega funciones para entidades"
    This reverts commit b3f69b89792740796a55e72f819110599929bc69.

--- src/entidades/paginas.js
+++
@@ -1,8 +0,0 @@
-export class Pagina{
-    constructor(respuestaPagina){
-        this.totalPokemones = respuestaPagina.count
-        this.proximaPagina = respuestaPagina.next
-        this.paginaSiguiente = respuestaPagina.previous
-        this.listaPokemones = respuestaPagina.results
-    }
-}
