741ff01e3a2a080c3f8081784ca5ae042ce558f5
juan <juancraft12yt@gmail.com>
2020-08-12 22:20:59 -0300

    actualiza nombre de las funciones
    cambia los nombres de las funciones y deja de usar abreviaturas

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,12 +1,12 @@
-export function guardarPokemon (pokemon) {
+export function guardarPokemon(pokemon) {
   localStorage.setItem(pokemon.nombre, JSON.stringify(pokemon))
 }
-export function buscarPokemonEnLS (pokemon) {
+export function buscarPokemonEnLocalStorage (pokemon) {
   return JSON.parse(localStorage.getItem(pokemon))
 }
 export function guardarPagina(offset, respuesta) {
   localStorage.setItem(offset, JSON.stringify(respuesta))
 }
-export function buscarPaginaEnLS (respuesta) {
+export function buscarPaginaEnLocalStorage(respuesta) {
   return JSON.parse(localStorage.getItem(respuesta))
 }
