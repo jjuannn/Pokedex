95bd69b35367497fab8dea4372657f9fdec519c0
juan <juancraft12yt@gmail.com>
2020-08-12 22:23:54 -0300

    Arregla errores varios
    cambia las constantes dentro de actualizarInformacion, para que la actualize con lo que le devuelve el mapeador.

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,19 +1,21 @@
-import { cargarDataPokemones } from '../servicios/servicios.js'
-import { buscarPokemonEnLS } from '../storage/storage.js'
+import { cargarPokemon } from '../servicios/servicios.js'
+import { buscarPokemonEnLocalStorage } from '../storage/storage.js'
 import { buscarPokemonEnApi, obtenerInfoPokemones } from '../api/api.js'
 
 export async function inicializar(){
   const infoPokemones = await obtenerInfoPokemones()
   borrarPokemonesAnteriores()
   mostrarCantidadPokemones(infoPokemones.totalPokemones)
-  crearListaPokemones(infoPokemones.listaPokemones)
+  crearListaPokemones(infoPokemones)
 }
 export function mostrarCantidadPokemones (cantidadPokemones) {
   document.querySelector('#cantidad-pokemones').textContent = cantidadPokemones
 }
-export async function crearListaPokemones (pokemones) {
+export async function crearListaPokemones(pokemones) {
   const $listaPokemones = document.querySelector('#lista-pokemones')
-    pokemones.forEach(pokemon => {
+  // PREGUNTAR SI listadoPokemones ESTA BIEN
+  const listadoPokemones = pokemones.listaPokemones
+    listadoPokemones.forEach(pokemon => {
     const { name: nombre } = pokemon
     const $link = document.createElement('a')
     $link.textContent = nombre
@@ -21,7 +23,7 @@ export async function crearListaPokemones (pokemones) {
     $link.className = 'list-group-item list-group-item-action l-pokemones'
     $link.classList.add(nombre)
     $link.addEventListener('click', async () => {
-      actualizarInformacion(await cargarDataPokemones(nombre))
+      actualizarInformacion(await cargarPokemon(nombre))
     })
     $listaPokemones.appendChild($link)
   })
@@ -53,18 +55,18 @@ function mostrarInfoPokemones (peso, altura, nombre, vida, ataque, defensa, ataq
   $velocidadPokemon.textContent = velocidad
 }
 
-export function actualizarInformacion(rtaJSON) {
-  const peso = rtaJSON.peso
-  const altura = rtaJSON.altura
-  const nombre = rtaJSON.nombre
-  const $foto = rtaJSON.fotos
-  const hp = Object.values(rtaJSON.estadisticas[0])[0]
-  const ataque = Object.values(rtaJSON.estadisticas[1])[0]
-  const defensa = Object.values(rtaJSON.estadisticas[2])[0]
-  const ataqueEspecial = Object.values(rtaJSON.estadisticas[3])[0]
-  const defensaEspecial = Object.values(rtaJSON.estadisticas[4])[0]
-  const velocidad = Object.values(rtaJSON.estadisticas[5])[0]
-  const tipoPokemon = rtaJSON.tipos
+function actualizarInformacion(pokemon){
+  const peso = pokemon.peso
+  const altura = pokemon.altura
+  const nombre = pokemon.nombre
+  const foto = pokemon.foto
+  const hp = pokemon.hp
+  const ataque = pokemon.ataque
+  const defensa = pokemon.defensa
+  const ataqueEspecial = pokemon.ataqueEspecial
+  const defensaEspecial = pokemon.defensaEspecial
+  const velocidad = pokemon.velocidad
+  const tipoPokemon = pokemon.tipos
   const $strongs = document.querySelectorAll('.tipos')
   $strongs.forEach((tipo) => {
     tipo.remove()
@@ -80,7 +82,7 @@ export function actualizarInformacion(rtaJSON) {
     $divs.appendChild($elementoTipoPokemon)
   })
   const $fotoPokemon = document.querySelector('#foto-pokemon')
-  $fotoPokemon.src = $foto
+  $fotoPokemon.src = foto
   $fotoPokemon.classList.remove('oculto')
   mostrarInfoPokemones(peso, altura, nombre, hp, ataque, defensa, ataqueEspecial, defensaEspecial, velocidad)
 }
