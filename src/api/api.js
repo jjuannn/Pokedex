b3616d1d7ab0e5f2225cb0b22073111a76dd2dea
juan <juancraft12yt@gmail.com>
2020-08-12 22:14:37 -0300

    agrega url guardada en una const
    agrega la const baseURL y las usa en los fetch para no escribir muchas veces la misma URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,51 +1,26 @@
 import {
-  mostrarCantidadPokemones,
-  crearListaPokemones,
-  borrarPokemonesAnteriores
-} from '../ui/ui.js'
-import {
-  guardarPokemon,
-  buscarPokemonEnLS,
   guardarPagina
 } from '../storage/storage.js'
-import {
-  offset
-} from '../manejador/manejador.js'
 import { nuevaPagina } from '../mapeador/mapeador.js'
 
+const baseURL = "https://pokeapi.co/api/v2/pokemon/"
+
 export function obtenerInfoPokemones () {
-  return fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
+  return fetch(`${baseURL}?offset=0&limit=20`)
     .then(respuesta => respuesta.json())
-    .then(respuestaJSON => {
-      const respuestaApi = respuestaJSON
+    .then(respuestaApi => {
       const primeraPagina = nuevaPagina(respuestaApi)
-      console.log(primeraPagina)
       guardarPagina("0", primeraPagina)
       return primeraPagina
     })
 }
 export function buscarPokemonEnApi(nombre) {
-    return fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
-      .then(rta => rta.json())
-      .then(rtaJSON => {
-        const respuestaPokemon = rtaJSON
-        return respuestaPokemon
-      })
+    return fetch(`${baseURL}${nombre}`)
+      .then(respuesta => respuesta.json())
 }
-export function obtenerPaginaSiguienteDesdeAPI () {
-  return fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`)
+ 
+export function obtenerPaginaSiguienteDesdeAPI(offset) {
+  return fetch(`${baseURL}?offset=${offset}&limit=20`)
     .then(respuesta => respuesta.json())
-    .then(respuestaJSON => {
-      const { results: pokemones } = respuestaJSON
-      return respuestaJSON
-    })
+}
 
-}
-export function obtenerPaginaAnteriorDesdeAPI () {
-  return fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`)
-    .then(respuesta => respuesta.json())
-    .then(respuestaJSON => {
-      const { results: pokemones } = respuestaJSON
-      return respuestaJSON
-    })
-}
